f217309b25b65ba248539e5a88a00cf230251a74
Yoni Lahav <yoni9093@gmail.com>
2016-10-22 16:08:10 +0300

    added support for viewing and managing multiple reminders

--- conv-bot.py
+++ conv-bot.py
@@ -5,14 +5,11 @@ from datetime import datetime, timedelta
 from reminder import Reminder
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardHide)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
-                          ConversationHandler, Job)
+                          ConversationHandler, Job, JobQueue)
 import logging, re, os
 
-if not os.path.exists('c:\\log\\TheOctobot\\conv-bot'):
-    os.makedirs('c:\\log\\TheOctobot\\conv-bot')
-
 # Enable logging
-logging.basicConfig(filename='c:\\log\\TheOctobot\\conv-bot\\log.txt',
+logging.basicConfig(filename='log.txt',
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
@@ -21,10 +18,18 @@ logger = logging.getLogger(__name__)
 EDIT, REDIRECT_REMINDER, REDIRECT_SETTING, SET_TIMER, CHOOSE_DAY, CHOOSE_TIME, ACTIVATE_TIMER = range(7)
 
 current_reminder = Reminder()
-active_reminders = [] # use this to manage a lot of reminders
+active_reminders = []
+
 
 def error(bot, update, error):
-    logger.warn('Update "%s" caused error "%s"' % (update, error))
+    logger.error('Update "%s" caused error "%s"' % (update, error))
+
+    bot.sendMessage(update.message.chat_id,
+                    text='An error has occurred. Please retry.',
+                    reply_markup=ReplyKeyboardHide())
+
+    return -1 # end conversation
+
 
 def set_reminder(bot, update):
 
@@ -46,27 +51,15 @@ def set_reminder(bot, update):
 
     return REDIRECT_REMINDER
 
-def edit(bot, update):
-    bot.sendMessage(update.message.chat_id, text='what do')
-
-def choose_time_setting(bot, update):
-    reply_keyboard = [['Specific time', 'Timer']]
-
-    bot.sendMessage(update.message.chat_id,
-                    text='Please choose a time setting.',
-                    reply_markup=ReplyKeyboardMarkup(reply_keyboard,
-                                                     one_time_keyboard=True))
-
-    return REDIRECT_SETTING
 
 def redirect_reminder(bot, update):
 
-    if (update.message.text == 'Ok'):
+    if update.message.text == 'Ok':
 
         bot.sendMessage(update.message.chat_id, text='Great!', reply_markup=ReplyKeyboardHide())
         return choose_time_setting(bot, update)
 
-    elif (update.message.text == 'Edit'):
+    elif update.message.text == 'Edit':
 
         bot.sendMessage(update.message.chat_id, text='No problem!\nJust enter a new message.',
                         reply_markup=ReplyKeyboardHide())
@@ -79,15 +72,27 @@ def redirect_reminder(bot, update):
                         reply_markup=ReplyKeyboardHide())
         return set_reminder(bot, update)
 
+
+def choose_time_setting(bot, update):
+    reply_keyboard = [['Specific time', 'Timer']]
+
+    bot.sendMessage(update.message.chat_id,
+                    text='Please choose a time setting.',
+                    reply_markup=ReplyKeyboardMarkup(reply_keyboard,
+                                                     one_time_keyboard=True))
+
+    return REDIRECT_SETTING
+
+
 def redirect_setting(bot, update):
 
-    if (update.message.text == 'Specific time'):
+    if update.message.text == 'Specific time':
 
         bot.sendMessage(update.message.chat_id, text='When do you wish to be reminded?',
                         reply_markup=ReplyKeyboardHide())
         return set_specific(bot, update)
 
-    elif (update.message.text == 'Timer'):
+    elif update.message.text == 'Timer':
 
         bot.sendMessage(update.message.chat_id, text='How long should I wait?\n'
                                                      'Please enter an amount in the following format:\n<number><s|m|h> (i.e. 30s)',
@@ -100,6 +105,7 @@ def redirect_setting(bot, update):
                         reply_markup=ReplyKeyboardHide())
         return choose_time_setting(bot, update)
 
+
 def set_specific(bot, update):
 
     current_reminder.use_date = True
@@ -111,6 +117,7 @@ def set_specific(bot, update):
 
     return CHOOSE_DAY
 
+
 def choose_day(bot, update):
 
     global current_reminder
@@ -118,19 +125,20 @@ def choose_day(bot, update):
     full_date_re = re.compile('^(\d{2}\/\d{2}\/\d{2})$')
     partial_date_re = re.compile('^(\d{2}\/\d{2})$')
 
-    if (update.message.text == 'Today'):
+    if update.message.text == 'Today':
         date = datetime.today()
-    elif (update.message.text == 'Tomorrow'):
+
+    elif update.message.text == 'Tomorrow':
         date = datetime.today() + timedelta(days=1)
+
     elif full_date_re.match(update.message.text):
         date = datetime.strptime(update.message.text, '%d/%m/%y')
+
     elif partial_date_re.match(update.message.text):
         date = datetime.strptime(update.message.text, '%d/%m')
         date.replace(year=datetime.today().year)
 
     current_reminder.reminder_date = date
-
-    # TODO: else: raise and handle
 
     bot.sendMessage(update.message.chat_id,
                     text='Next, please enter a time in the following format: HH:MM.',
@@ -173,8 +181,6 @@ def set_timer(bot, update):
     elif timer_type == 'h':
         timer = timedelta(hours=amount)
 
-    # TODO: else: raise exception + catch and show usage and perhaps retry if possible
-
     current_reminder.reminder_timer = timer
 
     # register reminder and set timer
@@ -199,12 +205,14 @@ def activate_timer(bot, update, job_queue):
         bot.sendMessage(update.message.chat_id,
                         text='This reminder was set in the past; Please enter a valid value next time.',
                         reply_markup=ReplyKeyboardHide())
-        return
+        return -1 # end conversation
+        #  or just return SET_TIMER again? but i would need to remember the decision for which time setting to use
 
     # Add job to queue
-    job = Job(reminder_alarm, due, repeat=False, context=dict(chat_id=update.message.chat_id,
-                                                              message=current_reminder.reminder_text))
+    job = create_job(current_reminder, update, due)
     job_queue.put(job)
+
+    active_reminders.append(current_reminder)
 
     logger.info('Added new reminer:\n%s' % current_reminder.to_string())
 
@@ -212,25 +220,107 @@ def activate_timer(bot, update, job_queue):
                     text='Reminder successfully set!',
                     reply_markup=ReplyKeyboardHide())
 
+    return -1 # end conversation
+
 
 def reminder_alarm(bot, job):
+
+    global active_reminders
+
     bot.sendMessage(job.context['chat_id'],
                     text='Reminding you to ' + job.context['message'],
                     reply_markup=ReplyKeyboardHide())
 
+    active_reminders = [reminder for reminder in active_reminders if reminder.id != job.context['reminder_id']]
+
 
 def cancel(bot, update):
-    bot.sendMessage(update.message.chat_id, text='Reminder cancelled.')
+    global current_reminder
+    if not current_reminder.is_empty():
+        bot.sendMessage(update.message.chat_id, text='Reminder cancelled.',
+                        reply_markup=ReplyKeyboardHide())
+        current_reminder = Reminder()
+    else:
+        bot.sendMessage(update.message.chat_id, text='There is no active reminder.',
+                        reply_markup = ReplyKeyboardHide())
+
+    return -1 # end conversation
+
+
+def show_actives(bot, update):
+    global active_reminders
+
+    if len(active_reminders) == 0:
+        bot.sendMessage(update.message.chat_id, text='There are no active reminders.')
+        return
+
+    all = str(len(active_reminders)) + ' reminders:\n'
+
+    for i in range(len(active_reminders)):
+        all += str(i+1) + ') ' + active_reminders[i].to_string_active() + '\n'
+
+    bot.sendMessage(update.message.chat_id, text=all)
+
+
+def bot_help(bot, update):
+    bot.sendMessage(update.message.chat_id,
+                    text='Hello!\nI am Remind Me Bot. Use me to set up reminders!\n\n'
+                         'To set up a new reminder, simply ask me to remind you!\n'
+                         'i.e Remind me to take out the trash\n\n'
+                         'Commands:\n'
+                         '/help - show help message\n'
+                         '/cancel - cancels current reminder\n'
+                         '/active - show all active reminders\n'
+                         '/stop - stop specific reminder (i.e. /stop 2 stops reminder number 2)')
+
+
+def create_job(reminder, update, due):
+    return Job(reminder_alarm, due, repeat=False, context=dict(chat_id=update.message.chat_id,
+                                                               message=reminder.reminder_text,
+                                                               reminder_id=reminder.id))
+
+
+def stop_reminder(bot, update, args, job_queue):
+
+    global active_reminders
+
+    try:
+        request = int(args[0])
+
+    except ValueError:
+        bot.sendMessage(update.message.chat_id,
+                        text='Please enter a valid reminder number.',
+                        reply_markup=ReplyKeyboardHide())
+        return -1
+
+    if request < 0 or request > len(active_reminders):
+        bot.sendMessage(update.message.chat_id,
+                        text='Please enter a valid reminder number.',
+                        reply_markup=ReplyKeyboardHide())
+        return -1
+
+    id = active_reminders[request - 1].id
+    [job for job in list(job_queue.jobs()) if job.context['reminder_id'] == id][0].schedule_removal()
+    active_reminders = [reminder for reminder in active_reminders if reminder.id != id]
+
+    bot.sendMessage(update.message.chat_id,
+                    text='Reminder %d stopped.' % request,
+                    reply_markup=ReplyKeyboardHide())
 
 
 def main():
-    # Create the EventHandler and pass it your bot's token.
+    # creating main event handler
     updater = Updater("291734070:AAG0u_LMBw9UdKmaAX1LqtDcLDTuWbUjCq4")
 
-    # Get the dispatcher to register handlers
+    # getting dispatcher to register handlers
     dp = updater.dispatcher
 
-    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    # command handlers
+    help = CommandHandler('help', bot_help)
+    show = CommandHandler('active', show_actives)
+    stop = CommandHandler('stop', stop_reminder, pass_args=True, pass_job_queue=True)
+
+    # main conversation handler
     conv_handler = ConversationHandler(
         entry_points=[RegexHandler(u'^remind me to\s|^remindme\s|^הזכר לי\s|^תזכיר לי\s', set_reminder)],
 
@@ -255,17 +345,19 @@ def main():
 
     conv_handler.allow_reentry = True
 
+    # registering handlers
     dp.add_handler(conv_handler)
+    dp.add_handler(help)
+    dp.add_handler(show)
+    dp.add_handler(stop)
 
     # log all errors
     dp.add_error_handler(error)
 
-    # Start the Bot
+    # starting bot
     updater.start_polling()
 
-    # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+    # running bot
     updater.idle()
 
 if __name__ == '__main__':
--- reminder.py
+++ reminder.py
@@ -1,5 +1,5 @@
-from time import strftime
-
+from datetime import datetime, timedelta
+import uuid
 
 class Reminder:
 
@@ -10,6 +10,8 @@ class Reminder:
         self.reminder_text = ''
         self.email_reminder = False
         self.email_address = ''
+        self.created = datetime.now()
+        self.id = uuid.uuid1()
 
     def to_string(self):
 
@@ -30,3 +32,33 @@ class Reminder:
             display += 'Email reminder: No'
 
         return display
+
+    def to_string_active(self):
+
+        display = 'Reminder: ' + self.reminder_text + '\n'
+
+        if self.use_date:
+            due = int((self.reminder_date - datetime.today()).total_seconds())
+        else:
+            due_date = self.created + self.reminder_timer
+            due = int((due_date - datetime.today()).total_seconds())
+
+        display += 'In ' + \
+                   str(int(due / 3600)) + ' hours, ' + \
+                   str(int((due % 3600) / 60)) + ' minutes and ' + \
+                   str(int((due % 3600) % 60)) + ' seconds\n'
+
+        if self.email_reminder:
+            display += 'Email reminder: Yes\nEmail address: ' + self.email_address
+        else:
+            display += 'Email reminder: No'
+
+        return display
+
+    def is_empty(self):
+        return (self.use_date == True and
+                self.reminder_date == None and
+                self.reminder_timer == None and
+                self.reminder_text == '' and
+                self.email_reminder == False and
+                self.email_address == '')
